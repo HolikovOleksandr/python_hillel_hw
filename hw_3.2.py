84df1761924fd9590ee947aa1c439d253be00eb5
Les Holikov <sanyagolikov97@gmail.com>
2025-03-24 11:47:05 +0200

    fixed hw_3.2 and hw_3.3

--- hw_3.2.py
+++ hw_3.2.py
@@ -1,17 +1,13 @@
-lst_a = [12, 3, 4, 10]     # [10, 12, 3, 4]
-lst_b = [1]                # [1]
-lst_c = []                 # []
-lst_d = [12, 3, 4, 10, 8]  # [8, 12, 3, 4, 10]
+lst = [12, 3, 4, 10]     # [10, 12, 3, 4]
+# lst = [1]                # [1]
+# lst = []                 # []
+# lst = [12, 3, 4, 10, 8]  # [8, 12, 3, 4, 10]
 
 
-def set_last_list_item_to_first(lst):
-    if len(lst) > 1:
-        lst.insert(0, lst.pop())
 
-    return lst
+if len(lst) > 1:
+    lst.insert(0, lst.pop())
 
 
-print("Expected [10, 12, 3, 4] => ", set_last_list_item_to_first(lst_a))
-print("Expected [1] => ", set_last_list_item_to_first(lst_b))
-print("Expected [] => ", set_last_list_item_to_first(lst_c))
-print("Expected [8, 12, 3, 4, 10] => ", set_last_list_item_to_first(lst_d))
+
+print("Expected [10, 12, 3, 4] => ", lst)
--- hw_3.3.py
+++ hw_3.3.py
@@ -1,18 +1,15 @@
-lst_a = [1, 2, 3, 4, 5, 6]  # [[1, 2, 3], [4, 5, 6]]
-lst_b = [1, 2, 3]           # [[1, 2], [3]]
-lst_c = [1, 2, 3, 4, 5]     # [[1, 2, 3], [4, 5]]
-lst_d = [1]                 # [[1], []]
-lst_e = []                  # [[], []]
+lst = [1, 2, 3, 4, 5, 6]  # [[1, 2, 3], [4, 5, 6]]
+# lst = [1, 2, 3]           # [[1, 2], [3]]
+# lst = [1, 2, 3, 4, 5]     # [[1, 2, 3], [4, 5]]
+# lst = [1]                 # [[1], []]
+# lst = []                  # [[], []]
 
 
-def split_list_into_halves(lst):
-    if not lst: return [[], []]
+if not lst: 
+    lst = [[], []]
+else:
     half = (len(lst) + 1) // 2
-    return [lst[:half], lst[half:]]
+    lst = [lst[:half], lst[half:]]
 
 
-print("Expected [[1, 2, 3], [4, 5, 6]] => ", split_list_into_halves(lst_a))
-print("Expected [[1, 2], [3]] => ", split_list_into_halves(lst_b))
-print("Expected [[1, 2, 3], [4, 5]] => ", split_list_into_halves(lst_c))
-print("Expected [[1], []] => ", split_list_into_halves(lst_d))
-print("Expected [[], []] => ", split_list_into_halves(lst_e))
+print("Expected [[1, 2, 3], [4, 5, 6]] => ", lst)
