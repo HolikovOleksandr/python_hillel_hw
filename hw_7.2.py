2f339f593971a8ae8e273258c5b71d8d801ef8fe
LesHolikov <sanyagolikov97@gmail.com>
2025-04-21 13:58:54 +0300

    hw 7 done

---
+++ hw_7.2.py
@@ -0,0 +1,14 @@
+def correct_sentence(sentence:str) -> str:
+    res = sentence[0].upper() + sentence[1:]
+
+    if not sentence.endswith('.'): 
+        res += '.'
+    
+    return res
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
